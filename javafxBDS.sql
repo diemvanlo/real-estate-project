21ae18ea306944e10c3ba53e19b1fa710fecc7cc
74nguyentan <exfick@gmail.com>
2019-11-30 08:55:17 +0700

    sql and image

--- javafxBDS.sql
+++ javafxBDS.sql
@@ -4,7 +4,7 @@ use javafxbds;
 
 
 create table Userr(
-IdUser int identity(0,1) primary key not null,
+IdUser int identity(1,1) primary key not null,
 NameUser nvarchar(50) not null,
 UserName varchar(50) not null,
 Password varchar(50) not null,
@@ -17,10 +17,12 @@ Role int not null,
 );
 
 insert into Userr(NameUser,UserName,Password,DiaChi,Sdt,Email,GioiTinh,ChucVu,Role) values
+(N'Đỗ Văn Liêm','VanLiem','123',N'Quảng bình','0968555678','VanLiem@gmail.com','true',N'Giám Đốc',0),
+(N'Nguyễn Thế Công','TheCong','123',N'Quảng bình','0522355667','thecong@gmail.com','true',N'Nhân Viên',2),
 (N'Nguyễn Văn Tân','VanTan','123',N'Quảng trị','0325772929','VanTan@gmail.com','true',N'Giám Đốc',1);
 
 create table KhachHang(
-IdKhachHang int identity(0,1) primary key not null,
+IdKhachHang int identity(1,1) primary key not null,
 TenKhachHang nvarchar(50) not null ,
 GioiTinh bit,
 DiaChi nvarchar(100) not null,
@@ -29,11 +31,18 @@ Email nvarchar(50),
 );
 
 insert into KhachHang(TenKhachHang,GioiTinh,DiaChi,Sdt,Email) values
+(N'Nguyễn thị Hằng','false',N'Quảng ninh','0955884667','hanga@gmail.com'),
+(N'Nguyễn thị Ngọc','false',N'Quảng trị','0955884667','Ngoc@gmail.com'),
+(N'Nguyễn thị F','false',N'Quảng trị','0955884667','kaca@gmail.com'),
+(N'Nguyễn Văn K','true',N'Quảng bình','032544777','jda@gmail.com'),
+(N'Nguyễn Văn C','false',N'Quảng trị','0622555884','abc@gmail.com'),
 (N'Nguyễn Văn A','true',N'Quảng trị','0327772999','Vana@gmail.com'),
 (N'Nguyễn Văn B','true',N'Quảng Bình','0566778998','Basd@gmail.com');
 
+
+
 create table DoiTac(
-IdDoiTac int identity(0,1) primary key not  null,
+IdDoiTac int identity(1,1) primary key not  null,
 TenDoiTac nvarchar(30) not null,
 LinhVuc nvarchar(30) ,
 DiaChi nvarchar(60) not null,
@@ -42,19 +51,22 @@ Email varchar(15),
 Logo Varbinary(max)  ,
 SoVonDaDauTu float,
 )
+
 insert into DoiTac(TenDoiTac,LinhVuc,DiaChi,Sdt,Email,SoVonDaDauTu) values
+(N'Cty AS',N'Xây Dựng',N'Quảng Nam','0325698756','koko@gmail.com',25445),
+(N'Cty FC',N'Xây Dựng',N'Quảng Trị','0321112546','ddf@gmail.com',45453),
+(N'Cty HANAM',N'Nhà Đất',N'Quảng Ngãi','0456888385','ffc@gmail.com',53536),
+(N'Cty NAITHANG',N'Xây Dựng',N'Quảng Ninh','0896888321','sfaac@gmail.com',2323),
+(N'Cty XD Phúc Hải',N'Bất Động Sản',N'Quảng bình','0999988321','sdc@gmail.com',36487),
+(N'Cty MiaAli',N'Xây Dựng',N'Quảng Trị','0456845631','fc@gmail.com',96533535);
+
+(N'Cty sdfsd',N'Xây Dựng',N'Quảng bình','0456888321','fsdfsdfc@gmail.com',645465),
+(N'Cty BBC',N'Bât động sản',N'Quảng Ngãi','05224448465','ab@gmail.com',455465),
 (N'Cty ABC',N'Xây Dựng',N'Quảng Nam','0325111222','abc@gmail.com',455465),
 (N'Cty BDS',N'Bất Động Sản',N'Quảng Ngãi','0326777988','dda@gmail.com',78826);
 
-create table Image(
-idImage int identity(0,1) primary key not  null,
-IdSanPham nvarchar(30) not null,
-ImageMode nvarchar(30) ,
-Image Varbinary(max)  ,
-)
-
 create table DuAn(
-IdDuAn int identity(0,1) primary key not null,
+IdDuAn int identity(1,1) primary key not null,
 TenDuAn nvarchar(50) not null,
 LoaiHinh nvarchar(30) not null,
 DiaChi nvarchar(50) not null,
@@ -72,12 +84,22 @@ BanKinh float,
 constraint fk_DuAn foreign key (IdDoiTac) references DoiTac(IdDoiTac), 
 )
 
+
 insert into DuAn(TenDuAn,LoaiHinh,DiaChi,DienTich,ChiPhi,NgayBatDau,NgayKetThuc,HinhThucQuanLi,HinhThucDauTu,IdDoiTac,TrangThai,MapX,MapY,BanKinh) values
-(N'Chung cư ABCD',N'Chung Cư',N'08 Hà văn Tính',112,550,'2017-05-02','',N'Chung Cư',N'góp vốn',0,N'Đang xây dựng',54545,6565,4545),
-(N'Chung cư sa',N'Chung Cư',N'03 Hà văn Ngọc',4545,10289,'2019-01-02','',N'Chung Cư',N'góp vốn ',0,N'Đang xây dựng',85698,5454,87887);
+
+(N'Chung cư AAA',N'Chung Cư',N'325 Ngô Văn Sở',78,23,'2017-05-29','',N'Chung Cư',N'góp vốn',1,N'Đang xây dựng',232,1112,5645),
+(N'Chung cư Liên Chiểu',N'Chung Cư',N'108 Bắc Tính',112,550,'2017-05-12','',N'Chung Cư',N'góp vốn',6,N'Đang xây dựng',698,786,7886),
+(N'Chung cư KHD',N'Chung Cư',N'08 Hà Huy Tập',7887,5345,'2019-09-02','',N'Chung Cư',N'góp vốn',8,N'Đang xây dựng',788,65,566),
+(N'Chung cư UIO',N'Chung Cư',N'699 Hà Huy Tập',655,868,'2019-08-02','',N'Chung Cư',N'góp vốn',5,N'Đang xây dựng',54545,5566,788),
+(N'Chung cư Hòa Khánh',N'Chung Cư',N'08 Bạch Đằng',785,988,'2015-05-02','',N'Chung Cư',N'góp vốn',2,N'Đang xây dựng',7878,686,4545),
+(N'Chung cư Hòa Minh',N'Chung Cư',N'32 Nam Sơn',112,565,'2017-05-02','',N'Chung Cư',N'góp vốn',4,N'Đang xây dựng',5963,6565,786),
+(N'Đất nền HA Nam',N'Đất nền',N'56 Hà văn Nam',533,785,'2017-05-09','',N'Đất nền',N'góp vốn',1,N'Đang xây dựng',213,355,532),
+
+(N'Chung cư ABCD',N'Chung Cư',N'08 Hà văn Tính',112,550,'2017-05-02','',N'Chung Cư',N'góp vốn',1,N'Đang xây dựng',54545,6565,4545),
+(N'Chung cư sa',N'Chung Cư',N'03 Hà văn Ngọc',4545,10289,'2019-01-02','',N'Chung Cư',N'góp vốn ',1,N'Đang xây dựng',85698,5454,87887);
 
 create table SanPham(
-IdSanPham int identity(0,1) primary key not null,
+IdSanPham int identity(1,1) primary key not null,
 TenSanPham nvarchar(50) not null,
 IdDuAn int not null,
 DiaChi nvarchar(50) not null,
@@ -94,8 +116,16 @@ constraint fk_DuAn_SP foreign key (IdDuAn) references DuAn(IdDuAn),
 )
 
 insert into SanPham(TenSanPham,IdDuAn,DiaChi,DienTich,GiaTien,MoTa,NgayTao,NgayBan,ChiTiet,trangThai,IdKhachHang) values
-(N'Phòng 101',0,N'08 Hà Văn Tính',50,510,N'đang xây dựng','2017-05-02','',N'phòng đầy đủ tiện nghi',N'chưa bán',''),
-(N'Phòng 02',1,N'03 Hà Văn Ngọc',40,390,N'đang xây dựng','2019-03-02','',N'phòng đầy đủ tiện nghi',N'chưa bán','');
+(N'Phòng 15',2,N'03 Hà Văn Tính',40,390,N'đang xây dựng','2019-03-02','',N'phòng đầy đủ tiện nghi',N'chưa bán',2),
+(N'Phòng 16',2,N'08 Bạch Đằng',40,390,N'đang xây dựng','2019-03-02','',N'phòng đầy đủ tiện nghi',N'chưa bán',2),
+(N'Phòng 17',2,N'03 Hà Văn Ngọc',40,390,N'đang xây dựng','2019-03-02','',N'phòng đầy đủ tiện nghi',N'chưa bán',2),
+(N'Phòng 18',2,N'03 Hà Văn Ngọc',40,390,N'đang xây dựng','2019-03-02','',N'phòng đầy đủ tiện nghi',N'chưa bán',2),
+(N'Phòng 19',2,N'03 Hà Văn Ngọc',40,390,N'đang xây dựng','2019-03-02','',N'phòng đầy đủ tiện nghi',N'chưa bán',2),
+(N'Phòng 20',2,N'03 Hà Văn Ngọc',40,390,N'đang xây dựng','2019-03-02','',N'phòng đầy đủ tiện nghi',N'chưa bán',2),
+(N'Phòng 21',2,N'03 Hà Văn Ngọc',40,390,N'đang xây dựng','2019-03-02','',N'phòng đầy đủ tiện nghi',N'chưa bán',2),
+
+(N'Phòng 101',1,N'08 Hà Văn Tính',50,510,N'đang xây dựng','2017-05-02','',N'phòng đầy đủ tiện nghi',N'chưa bán',1),
+(N'Phòng 02',2,N'03 Hà Văn Ngọc',40,390,N'đang xây dựng','2019-03-02','',N'phòng đầy đủ tiện nghi',N'chưa bán',2);
 
 create table DanhGia(
 IdKhachHang int not null,
@@ -108,12 +138,17 @@ unique (IdKhachHang,IdSanPham)
 );
 
 create table HinhAnh(
-IdHinhAnh int identity(0,1) primary key not null,
+IdHinhAnh int identity(1,1) primary key not null,
 HinhAnh varchar(50) ,
 CheDo nvarchar(20) , 
 IdSanPham int not null,
 constraint fk_HinhAnh foreign key (IdSanPham) references SanPham(IdSanPham),
 )
+
+insert HinhAnh(HinhAnh,CheDo,IdSanPham) values
+('1.jpg','Chế độ 360',3),
+('E:\Users\IdeaProjects\img\2.jpg','Chế độ 360',2);
+
 
 
 /*   sp_ThongKeDA    */
